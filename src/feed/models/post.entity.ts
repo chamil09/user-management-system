278225084f59183ceaf3adf2e375da43ddca40c2
chamil09 <chamil.mech@gmail.com>
2023-01-18 14:33:31 +0530

    init

---
+++ src/feed/models/post.entity.ts
@@ -0,0 +1,16 @@
+import {Column,PrimaryGeneratedColumn,Entity} from 'typeorm';
+
+@Entity('feed_post')
+export class FeedPostEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ default: '' })
+    body: string;
+
+    @Column({ type: 'timestamp', default:  () => 'CURRENT_TIMESTAMP'})
+    createdAt: Date;
+
+
+}
+
